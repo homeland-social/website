30700d755ca1ad2f2cea280059437cd75619fa28
Ben Timby <btimby@gmail.com>
2022-03-11 22:01:21 -0500

    Set up database config.

--- docker/powerdns/entrypoint.sh
+++ docker/powerdns/entrypoint.sh
@@ -15,8 +15,13 @@ PDNS_LOG_LEVEL=${PDNS_LOG_LEVEL:-4}
 
 envsubst < ${PDNS_CONF}.tmpl > ${PDNS_CONF}
 
-if [ ! -z "${SQLITE_DB}" ] && [ ! -f "${SQLITE_DB}" ]; then
+if [ ! -z "${SQLITE_DB}" ]; then
 	envsubst < ${PDNS_CONF_DIR}/sqlite.conf.tmpl > ${PDNS_CONF_DIR}/sqlite.conf
+elif [! -z "${PGSQL_HOST}" ]; then
+	envsubst < ${PDNS_CONF_DIR}/pgsql.conf.tmpl > ${PDNS_CONF_DIR}/pgsql.conf
+fi
+
+if [ ! -z "${SQLITE_DB}" ] && [ ! -f "${SQLITE_DB}" ]; then
 	sqlite3 ${SQLITE_DB} < ${PDNS_SQL_DIR}/schema.sqlite3.sql
 	chown -R pdns:pdns $(dirname ${SQLITE_DB})
 
@@ -24,8 +29,7 @@ if [ ! -z "${SQLITE_DB}" ] && [ ! -f "${SQLITE_DB}" ]; then
 		sqlite3 ${SQLITE_DB} "insert into supermasters values ('${PDNS_MASTER_ADDR}', '${PDNS_MASTER_NAME}', 'admin');"
 	fi
 
-elif [ ! -z "${PGSQL_HOST}" ] && [ "${PDNS_SLAVE}}" == "yes" ]; then
-	envsubst < ${PDNS_CONF_DIR}/pgsql.conf.tmpl > ${PDNS_CONF_DIR}/pgsql.conf
+elif [ ! -z "${PGSQL_HOST}" ] && [ "${PDNS_SLAVE}" == "yes" ]; then
 	psql -h ${PGSQL_HOST} -p ${PGSQL_PORT} -U ${PGSQL_USERNAME} -W "${PGSQL_PASSWORD}" ${PGSQL_DATABASE} 'insert into supermasters values ('${PDNS_MASTER_ADDR}', '${PDNS_MASTER_HOST}', 'admin');'
 
 fi
