37dd480ea8a50d5756a1de1aad74f880c2097373
Ben Timby <btimby@gmail.com>
2022-03-14 12:57:53 -0400

    Configure redis properly.

--- back/back/settings.py
+++ back/back/settings.py
@@ -118,11 +118,13 @@ DATABASES = {
     }
 }
 
+REDIS_HOST = os.getenv('REDIS_HOST', 'website-redis')
+REDIS_PORT = int(os.getenv('REDIS_PORT', '6379'))
 
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://redis:6379/1',
+        'LOCATION': f'redis://{REDIS_HOST}:{REDIS_PORT}/1',
     }
 }
 
@@ -178,7 +180,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'api.User'
 SESSION_ENGINE='django.contrib.sessions.backends.cached_db'
 
-CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/0')
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', f'redis://{REDIS_HOST}:{REDIS_PORT}/0')
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 CELERY_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
